f41b52a56d0aa7213b8eee938fb74bc4149ccc50
João Augusto Zanardo de Lima <augustozanardo@yahoo.com.br>
2022-11-11 04:31:41 -0300

    Frame compression

--- useHuffman.py
+++ useHuffman.py
@@ -8,5 +8,5 @@ h = HuffmanCoding(path)
 output_path = h.compress()
 print("Compressed file path: " + output_path)
 
-decom_path = h.decompress(output_path)
-print("Decompressed file path: " + decom_path)
+#decom_path = h.decompress(output_path)
+#print("Decompressed file path: " + decom_path)
